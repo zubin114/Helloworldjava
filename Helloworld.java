e5a3d36a6a4ab7ab061d212589c8e1205bea4391
zubin114 <104053006+zubin114@users.noreply.github.com>
2025-11-16 22:19:26 +0530

    Change greeting message in Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("HELLO Subin, World!");
+        System.out.println("HELLO Subin, Welcome!");
     }
 }
